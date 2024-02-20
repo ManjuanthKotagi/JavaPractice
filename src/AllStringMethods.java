01085c82b92304f3ad2cd9b21f81572cecd91809
Manjunath Kotagi <manjunath.kotagi@intimetec.com>
2024-02-20 16:39:45 +0530

    First Push

---
+++ src/AllStringMethods.java
@@ -0,0 +1,11 @@
+public class AllStringMethods {
+
+    public static void main(String[] args) {
+        
+        String str = "I love java and Selenium";
+        
+        //check the length of the string
+        int strLength = str.length();
+        System.out.println(strLength);
+    }
+}
