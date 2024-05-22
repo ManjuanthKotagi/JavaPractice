b41bfec049ed8aebff5e9623d7369174aa67c80c
Manjunath Kotagi <manjunath.kotagi@intimetec.com>
2024-05-22 08:24:45 +0530

    New Codes Pushed

---
+++ src/OccuranceOfCharacter.java
@@ -0,0 +1,23 @@
+public class OccuranceOfCharacter {
+
+    public static void main(String[] args) {
+        String str = "Welcome to Wipro";
+        char[] x = str.toCharArray();
+        int size = x.length;
+        int i=0;
+        int j=0;
+        int count =0;
+        while(i!=size){
+            j=i;
+            while(j<size){
+                if(x[i]==x[j]){
+                    count++;
+                }
+                j++;
+            }
+            System.out.println("Count of the "+ x[i] + " is : " + count);
+            count = 0;
+            i++;
+        }
+    }
+}
---
+++ src/SwappingString.java
@@ -0,0 +1,13 @@
+public class SwappingString {
+    public static void main(String[] args) {
+        String a = "coding";
+        String b = "ninjas";
+        System.out.println("Before Swapping " + a  +b);
+        a = a+b;
+        System.out.println("Concatinated String is " + a);
+        System.out.println(a.length()-b.length());
+        b = a.substring(0, a.length()-b.length());
+        a = a.substring(b.length());
+        System.out.println("After swap : " + a + b);
+    }
+}
